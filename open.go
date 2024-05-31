b713bb236b1b9c98dfadba722679c7712cd632fe
Keith Zantow <kzantow@gmail.com>
2024-05-31 00:12:03 -0400

    fix: OpenBrowser, add utils
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- open.go
+++ open.go
@@ -6,35 +6,18 @@ import (
 	"strings"
 )
 
-// OpenBrowser opens the specified URL in the default browser of the user.
-// https://stackoverflow.com/questions/39320371/how-start-web-server-to-open-page-in-browser-in-golang
+// OpenBrowser opens the specified URL in the user's default browser
 func OpenBrowser(url string) error {
-	var cmd string
-	var args []string
-
-	switch runtime.GOOS {
-	case "windows":
-		cmd = "cmd"
-		args = []string{"/c", "start"}
-	case "darwin":
-		cmd = "open"
+	var cmd []string
+	switch {
+	case runtime.GOOS == "windows" || isWSL():
+		cmd = []string{"cmd.exe", "/c", "start"}
+	case runtime.GOOS == "darwin":
+		cmd = []string{"open"}
 	default: // "linux", "freebsd", "openbsd", "netbsd"
-		// Check if running under WSL
-		if isWSL() {
-			// Use 'cmd.exe /c start' to open the URL in the default Windows browser
-			cmd = "cmd.exe"
-			args = []string{"/c", "start", url}
-		} else {
-			// Use xdg-open on native Linux environments
-			cmd = "xdg-open"
-			args = []string{url}
-		}
+		cmd = []string{"xdg-open"}
 	}
-	if len(args) > 1 {
-		// args[0] is used for 'start' command argument, to prevent issues with URLs starting with a quote
-		args = append(args[:1], append([]string{""}, args[1:]...)...)
-	}
-	return exec.Command(cmd, args...).Start()
+	return exec.Command(cmd[0], append(cmd[1:], url)...).Start()
 }
 
 // isWSL checks if the Go program is running inside Windows Subsystem for Linux
--- util.go
+++ util.go
@@ -34,3 +34,22 @@ func SortedMapValues[T any](m map[string]T) []T {
 	}
 	return out
 }
+
+func Map[From any, To any](slice []From, f func(f From) To) []To {
+	var to []To
+	for _, from := range slice {
+		to = append(to, f(from))
+	}
+	return to
+}
+
+func Filter[T any](slice []T, f func(f T) bool) []T {
+	var out []T
+	for _, t := range slice {
+		if !f(t) {
+			continue
+		}
+		out = append(out, t)
+	}
+	return out
+}
